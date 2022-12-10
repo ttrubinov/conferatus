159dd16079bf432e8d2d7026463bf1f8c38131d1
MihailCCfit <m.tsukanov@g.nsu.ru>
2022-12-10 13:59:52 +0700

    hello world, it's filters for microphones now

--- 2.c.ino
+++ 2.c.ino
@@ -5,45 +5,40 @@ static int a5f = 0, a6f = 0, a7f = 0;
 void setup() {
     Serial.begin(9600);
 }
- 
+
 void loop() {
-    sum = 0;
+
+    sum_a = 0;
+    sum_b = 0;
+    sum_c = 0;
     for (int i=0; i<4; i++){
-        a = analogRead(A7);
-        b = analogRead(A7);
-        c = analogRead(A7);
-        
-//        a -= 250;
-//        
-//        if (a < 0) {
-//          a = -a;
-//        }
-//    
-//        b -= 250;
-//        if (b < 0) {
-//          b = -b;
-//        }
-//    
-//    
-//        c -= 250;
-//        if (c < 0) {
-//          c = -c;
-//        }
-        
-        middle = (a < b) ? ((b < c) ? b : ((c < a) ? a : c)) : ((a < c) ? a : ((c < b) ? b : c));
-        sum+=middle;
-        lll += (a - lll) / 5;
-        lll += (b - lll) / 5;
-        lll += (c - lll) / 5;
+        a1 = analogRead(A5);
+        b1 = analogRead(A6);
+        c1 = analogRead(A7);
+        a2 = analogRead(A5);
+        b2 = analogRead(A6);
+        c2 = analogRead(A7);
+        a3 = analogRead(A5);
+        b3 = analogRead(A6);
+        c3 = analogRead(A7);
+        middle_a = (a1 < a2) ? ((a2 < a3) ? a2: ((a3 < a1) ? a1: a3)): ((a1 < a3) ? a1: ((a3 < a2) ? a2: a3));
+        sum_a += middle_a;
+        middle_b = (b1 < b2) ? ((b2 < b3) ? b2: ((b3 < b1) ? b1: b3)): ((b1 < b3) ? b1: ((b3 < b2) ? b2: b3));
+        sum_b += middle_b;
+        middle_c = (c1 < c2) ? ((c2 < c3) ? c2: ((c3 < c1) ? c1: c3)): ((c1 < c3) ? c1: ((c3 < c2) ? c2: c3));
+        sum_c += middle_c;
     }
-
-    sum = sum >> 2; // "/4"
-    
-    eee += (sum - eee) / 5;
-
-//    Serial.print(lll);
-//    Serial.print(" ");
-    Serial.println(eee);
+    sum_a = sum_a/4;
+    avexp_a += (sum_a - avexp_a) / 5;
+    sum_b = sum_b/4;
+    avexp_b += (sum_b - avexp_b) / 5;
+    sum_c = sum_c/4;
+    avexp_c += (sum_c - avexp_c) / 5;
+    Serial.print(avexp_c);
+    Serial.print("");
+    Serial.print(avexp_b);
+    Serial.print("");
+    Serial.print(avexp_c);
 
     delay(10);
 }
