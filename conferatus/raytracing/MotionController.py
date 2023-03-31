9676503deca4de9fd3a938c949e4f64b1a046eb0
ttrubinov <t.trubinov@g.nsu.ru>
2023-03-31 22:17:32 +0700

    Updater

--- conferatus/raytracing/MotionController.py
+++ conferatus/raytracing/MotionController.py
@@ -3,19 +3,17 @@ class MotionController:
         self.angle = start_angle
         self.coefficient = coefficient
 
-    def __update_with_new_value(self, new_value: float):
-        self.angle = self.coefficient * new_value + (1 - self.coefficient) * self.angle
-
     def __get_new_value(self):
         return self.angle
 
-    def whether_to_spin(self, new_angle: float) -> bool:
-        self.__update_with_new_value(new_angle)
+    def update_with_new_value(self, new_value: float):
+        self.angle = self.coefficient * new_value + (1 - self.coefficient) * self.angle
+
+    def whether_to_spin(self) -> bool:
         final_angle = self.__get_new_value()
         if abs(self.angle - final_angle) < 15:
             return False
         return True
 
     def get_new_rotation_angle(self):
-        self.__get_new_value()
-        return self.angle
+        return self.__get_new_value()
