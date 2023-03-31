eeaeb990ba84adaa565cb5bd46e2b8970f546507
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-03-31 14:45:08 +0700

    mvp structure

--- python/Gui.py
+++ python/Gui.py
@@ -1,40 +1,103 @@
+from dataclasses import dataclass
 from PyQt6 import QtWidgets, uic
 import sys
 
 from Arduino import ArduinoController
 
-class Ui(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(Ui, self).__init__()
+@dataclass(frozen=True)
+class UserDefinedParameters:
+    port: str
+    filename: str
+    person: str
+
+    frequency: int
+    batchSize: int
+    angle: int
+
+
+class SettingsPresenter:
+    def setSettingsWindowsView(self, settingsWindowView):
+        self.__settingsWindowView__ = settingsWindowView
+
+    def setSettingsModel(self, settingsModel):
+        self.__settingsModel__ = settingsModel
+
+    def startReadingSignal(self):
+        self.__settingsWindowView__.applyButton.setEnabled(False)
+        self.__settingsModel__.readSamples(self.__settingsWindowView__.getUserDefinedParameters())
+
+    def finishedReadingSignal(self):
+        self.__settingsWindowView__.applyButton.setEnabled(True)
+
+
+
+class SettingsWindowView(QtWidgets.QMainWindow):
+    __settingsPresenter__ : SettingsPresenter
+
+    def __init_widgets__(self):
+        self.portLineEdit = self.findChild(QtWidgets.QLineEdit, 'portLineEdit')
+        self.filenameLineEdit = self.findChild(QtWidgets.QLineEdit, 'filenameLineEdit')
+        self.personLineEdit = self.findChild(QtWidgets.QLineEdit, 'personLineEdit')
+
+        self.frequencySpinBox = self.findChild(QtWidgets.QSpinBox, 'frequencySpinBox')
+        self.batchSizeSpinBox = self.findChild(QtWidgets.QSpinBox, 'batchSizeSpinBox')
+        self.angleSpinBox = self.findChild(QtWidgets.QSpinBox, 'angleSpinBox')
+
+        self.applyButton = self.findChild(QtWidgets.QPushButton, 'applyButton')
+        self.applyButton.clicked.connect(self.__applyButtonPressed__) 
+
+    def __init__(self, settingsPresenter : SettingsPresenter):
+        super(SettingsWindowView, self).__init__()
         uic.loadUi('ui/settingsWindow.ui', self)
 
-        self.portLineEdit = self.findChild(QtWidgets.QLineEdit, 'portLineEdit')
-        self.frequencySpinBox = self.findChild(QtWidgets.QSpinBox, 'frequencySpinBox')
-
-        self.batchSizeSpinBox = self.findChild(QtWidgets.QSpinBox, 'batchSizeSpinBox')
-
-        self.applyButton = self.findChild(QtWidgets.QPushButton, 'applyButton') # Find the button
-        self.applyButton.clicked.connect(self.applyButtonPressed) 
+        self.__init_widgets__()
+        self.__settingsPresenter__ = settingsPresenter
 
         self.show()
 
-    def applyButtonPressed(self):
+    def __applyButtonPressed__(self):
+        self.__settingsPresenter__.startReadingSignal()
+
+    def getUserDefinedParameters(self) -> UserDefinedParameters:
         port = self.portLineEdit.text()
+        filename = self.filenameLineEdit.text()
+        person = self.personLineEdit.text()
+
         frequency = self.frequencySpinBox.value()
         batchSize = self.batchSizeSpinBox.value()
+        angle = self.angleSpinBox.value()
 
-        print(port)
-        print(frequency)
-        print(batchSize)
+        return UserDefinedParameters(port, filename, person, frequency, batchSize, angle)
+
+
+
+
+class SettingsModel:
+    def __init__(self, settingsPresenter : SettingsPresenter):
+        self.__settingsPresenter__ = settingsPresenter
+
+    def readSamples(self, userDefinedParameters : UserDefinedParameters):
+        params = userDefinedParameters
+        print(params)
+
+        # arduinoController = ArduinoController(batch_size = params.batchSize, port = params.port)
+        # arduinoController.recordData()
+
+        self.__settingsPresenter__.finishedReadingSignal()
+
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = Ui()
-    app.exec()
 
-    with ArduinoController(3, 230400, 1) as controller:
-        samples = controller.recordSamples()
+    presenter = SettingsPresenter()
+    window = SettingsWindowView(presenter)
+    model = SettingsModel(presenter)
+
+    presenter.setSettingsModel(model)
+    presenter.setSettingsWindowsView(window)
+
+    app.exec()
 
 if __name__ == '__main__':
     main()
