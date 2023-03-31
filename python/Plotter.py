9321596ea8f9cb7454333fe7f4cb3b847cc7d919
ttrubinov <t.trubinov@g.nsu.ru>
2023-03-31 14:52:48 +0700

    Draw any number of graphs

--- python/Plotter.py
+++ python/Plotter.py
@@ -2,13 +2,23 @@ from matplotlib import pyplot as plt
 
 
 class Plotter:
-    def show(*args, xlim=None, legend=None, title="", color="black"):
-        plt.plot(args, color)
-        plt.title(title)
-        if xlim is not None:
-            plt.xlim(0, xlim)
-        if legend is not None:
-            plt.legend(legend)
-        plt.grid()
-        plt.show()
-        plt.clf()
+    def draw(self, arrX: list[float], arrY: list[list[float]], color: list[str] = None, name: list[str] = None,
+             xlim: int = None, legend: str = None):
+        def show(plotTitle: str = ""):
+            plt.title(plotTitle)
+            if xlim is not None:
+                plt.xlim(0, xlim)
+            if legend is not None:
+                plt.legend(legend)
+            plt.grid()
+            plt.show()
+            plt.clf()
+
+        if color is not None and name is not None and len(arrY) == len(color) == len(name):
+            for data, clr, title in zip(arrY, color, name):
+                plt.plot(arrX, data, color=clr)
+                show(title)
+        else:
+            for data in arrY:
+                plt.plot(arrX, data, color="b")
+                show()
