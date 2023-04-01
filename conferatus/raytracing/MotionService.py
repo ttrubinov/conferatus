666c14f9b1f2693fe6d8db9ba7025a6ce29b00f9
ttrubinov <t.trubinov@g.nsu.ru>
2023-04-01 15:08:21 +0700

    Renaming

---
+++ conferatus/raytracing/MotionService.py
@@ -0,0 +1,18 @@
+class MotionController:
+    def __init__(self, start_angle: float = 0, coefficient: float = 0.5):
+        self.angle = start_angle
+        self.coefficient = coefficient
+
+    def __get_value(self):
+        return self.angle
+
+    def update_value(self, new_value: float):
+        self.angle = self.coefficient * new_value + (1 - self.coefficient) * self.angle
+
+    def whether_to_spin(self) -> bool:
+        if abs(self.angle - self.__get_value()) < 15:
+            return False
+        return True
+
+    def get_rotation_angle(self):
+        return self.__get_value()
