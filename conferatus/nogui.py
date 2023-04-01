35ad7acc1da153b6487abe466478b9dba0d96207
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-04-01 17:24:47 +0700

    быафвщл

--- conferatus/nogui.py
+++ conferatus/nogui.py
@@ -3,13 +3,12 @@ from raytracing.Fourier import Fourier
 from raytracing.ArduinoController import ArduinoController
 
 
-with ArduinoController(batch_size = 5, port = "/dev/ttyUSB0") as arduinoController:
+with ArduinoController(batch_size = 2, port = "/dev/ttyUSB0") as arduinoController:
     data = arduinoController.recordData()
 
-    for i in range(0, 5):
+    for i in range(0, 2):
         fourierSample = Fourier.get_amplitudes_and_phases(data[i])
-        # for i in fourierSample:
-        #     print(i)
-        print(fourierSample)
-
         Plotter.draw(y_axis=fourierSample)
+        # print(fourierSample)
+
+        
