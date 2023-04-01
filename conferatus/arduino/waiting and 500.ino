bf5baf608b9774c80bfae91b8f53a4d6921b882d
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-04-01 10:24:36 +0700

    add arduino code. Starting model

---
+++ conferatus/arduino/waiting and 500.ino
@@ -0,0 +1,214 @@
+#define MIC_PIN_0 A1
+#define MIC_PIN_1 A4
+#define MIC_PIN_2 A7
+#define Led A3
+
+#ifndef clearBit
+#define clearBit(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+
+#ifndef setBit
+#define setBit(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+#define T 1000000
+#define WAIT_LIMIT 10
+#define VOL_THRESHOLD 80
+#define VOL_SILENCE 40
+#define minSoundCount 4
+#define RememberMax 250
+#define bias 250
+
+
+#define bitADPS0 1
+#define bitADPS1 0
+#define bitADPS2 0
+
+#define delayM 0
+static bool running = false;
+void setup() {
+  pinMode(MIC_PIN_0, INPUT);
+  pinMode(MIC_PIN_1, INPUT);
+  pinMode(MIC_PIN_2, INPUT);
+  pinMode(Led, OUTPUT);
+
+#if bitADPS0 == 0
+  clearBit(ADCSRA, ADPS0);
+#else
+  setBit(ADCSRA, ADPS0);
+#endif
+#if bitADPS1 == 0
+  clearBit(ADCSRA, ADPS1);
+#else
+  setBit(ADCSRA, ADPS1);
+#endif
+#if bitADPS0 == 0
+  clearBit(ADCSRA, ADPS2);
+#else
+  setBit(ADCSRA, ADPS2);
+#endif
+  attachInterrupt(digitalPinToInterrupt(2), button, RISING);
+  Serial.begin(230400);
+}
+void button() {
+  running = false;
+}
+
+
+static int index = 0;
+static int vol[3][RememberMax];
+static String str;
+
+static bool ready = true;
+static int silence_counter=0;
+int vol1 =0;
+int vol2 =0;
+int vol3 =0;
+void loop() {
+  while(1){
+    //start and continue
+    if (Serial.available() > 0) {  //
+      // start
+      str = Serial.readString();
+      if (str[0] == 'i') {
+        digitalWrite(Led, HIGH);
+        running = true;
+      }
+      if (str[0] == 'c'){
+        // digitalWrite(Led, HIGH);
+        running = true;
+      }
+      if (str[0] == 's'){
+        digitalWrite(Led, LOW);
+        running = false;
+      }
+    }
+    if (running){
+      Serial.println("S");
+        // Finding silence
+      bool silence = false;
+      silence_counter = 0;
+      index = 0;
+      while(!silence){
+        vol[0][index] = analogRead(MIC_PIN_0) - bias;
+        vol[1][index] = analogRead(MIC_PIN_1) - bias;
+        vol[2][index] = analogRead(MIC_PIN_2) - bias;
+        if (abs(vol[0][index]) < VOL_SILENCE){
+          silence_counter++;
+        }
+        else{
+          silence_counter = 0;
+        }
+        if (abs(vol[1][index]) < VOL_SILENCE){
+          silence_counter++;
+        }
+        else{
+          silence_counter = 0;
+        }
+        
+        if (abs(vol[2][index]) < VOL_SILENCE){
+          silence_counter++;
+        }
+        else{
+          silence_counter = 0;
+        }
+
+        index++;
+        if (index == RememberMax){
+          index = 0;
+        }
+        if (silence_counter >= (3 * RememberMax)){
+          silence = true;
+        }
+      }
+      digitalWrite(Led, HIGH);
+      //Ищет превышение звука
+      int prev1=0, prev2=0, prev3=0;
+      while(1){
+        
+        vol[0][index] = analogRead(MIC_PIN_0) - bias;
+        vol[1][index] = analogRead(MIC_PIN_1) - bias;
+        vol[2][index] = analogRead(MIC_PIN_2) - bias;
+        
+        if (abs(vol[0][index]) >= VOL_THRESHOLD){
+          prev1++;
+          if (prev1== minSoundCount){
+            break;
+          }
+        }
+        else{
+          prev1 = 0;
+        }
+        
+        if (abs(vol[1][index]) >= VOL_THRESHOLD){
+        prev2++;
+          if (prev2== minSoundCount){
+            break;
+          }
+        }
+        else{
+          prev2 = 0;
+        }
+      
+        if (abs(vol[2][index]) >= VOL_THRESHOLD){
+          prev3++;
+          if (prev3== minSoundCount){
+            break;
+          }
+        }
+        else{
+          prev3 = 0;
+        }
+        
+        index++;
+        if (index >= RememberMax){
+          index = 0;
+        }
+      }
+      int count = 0;
+      while (count < RememberMax - minSoundCount){
+        vol[0][index] = analogRead(MIC_PIN_0) - bias;
+        vol[1][index] = analogRead(MIC_PIN_1) - bias;
+        vol[2][index] = analogRead(MIC_PIN_2) - bias;
+        index++;
+        if (index >= RememberMax){
+          index = 0;
+        }
+        count++;
+      }
+
+      int start = index;
+      index+=1;
+      if (index == RememberMax){
+        index = 0;
+      }
+      digitalWrite(Led, LOW);
+      while (start != index){
+        Serial.print(vol[0][index]);
+        Serial.print(" ");
+        Serial.print(vol[1][index]);
+        Serial.print(" ");
+        Serial.println(vol[2][index]);
+
+        index++;
+        if (index >= RememberMax){
+          index = 0;
+        }
+      }
+      Serial.print(vol[0][index]);
+      Serial.print(" ");
+      Serial.print(vol[1][index]);
+      Serial.print(" ");
+      Serial.println(vol[2][index]);
+
+
+      // Serial.flush();
+      Serial.println("E");
+      running=false;
+      digitalWrite(Led, LOW);
+    }
+    // else{
+    //   delay(1000);
+    // }
+  }
+}
--- conferatus/raytracing/MachineLearning.py
+++ conferatus/raytracing/MachineLearning.py
@@ -4,36 +4,156 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from tensorflow.keras import layers
+import tensorflow as tf
 
 
 class Model:
 
-    def __init__(self, sample_size=250, mic_amount=3):
-        inputs = keras.Input(shape=(784,), name="digits")
-        x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
-        x = layers.Dense(64, activation="relu", name="dense_2")(x)
-        outputs = layers.Dense(10, activation="softmax", name="predictions")(x)
+    def __init__(self, sample_size=250, mic_amount=3, has_person=False, has_frequency=True):
+        inputs = keras.Input(shape=(mic_amount, sample_size), name="microphones")
+        lay = layers.Dense(64, activation="relu", name="dense_1")(inputs)
+        lay = layers.Dense(64, activation="relu", name="dense_2")(lay)
+        predictions = layers.Dense(4, activation="softmax", name="predictions")(lay)
+        angle = layers.Dense(1, activation="sigmoid", name="angle")(lay)
+        frequency = layers.Dense(1, activation="sigmoid", name="frequency")(lay)
+        person = layers.Dense(1, activation="sigmoid", name="frequency")(lay)
 
     pass
 
 
-# x = np.arange(0, 10, 0.2, dtype=float)
-# y = x > 5 + 0
-# X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.75)
-# print(X_test, X_train, y_test, y_train)
-#
-# model = keras.Sequential([
-#     layers.Dense(4, input_dim=1, activation='relu'),
-#     layers.Dense(5, activation='relu'),
-#     layers.Dense(1, activation='sigmoid')
-# ])
+image_input = keras.Input(shape=(3, 250), name="microphones")
+# timeseries_input = keras.Input(shape=(None, 10), name="ts_input")
 
-inp = np.array([[1, 0, 0], [1, 0, 0], [1, 0, 0]])
-model = keras.Sequential()
-model.add(keras.Input(shape=3))
-model.add(layers.Dense(2, activation="relu"))
-print(model.summary())
-print(model(inp))
+x1 = layers.Dense(3)(image_input)
+# x1 = layers.Dense()(x1)
 
-# model.compile(loss='binary_crossentropy', optimizer='adam', jit_compile=True)
-# history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2000, batch_size=10)
+# x2 = layers.Conv1D(3, 3)(timeseries_input)
+# x2 = layers.GlobalMaxPooling1D()(x2)
+
+# x = layers.concatenate([x1, x2])
+
+score_output = layers.Dense(1, name="score_output")(x1)
+# class_output = layers.Dense(5, name="class_output")(x)
+
+model = keras.Model(
+    inputs=[image_input], outputs=[score_output]
+)
+model.compile(
+    optimizer=keras.optimizers.RMSprop(1e-3),
+    loss=[keras.losses.MeanSquaredError(), keras.losses.CategoricalCrossentropy()],
+)
+score_targets = np.random.random_sample(size=(1, 1))
+model.fit((dt := np.random.random_sample(size=(1, 3, 250))), score_targets)
+print(dt)
+input()
+
+
+def get_uncompiled_model():
+    inputs = keras.Input(shape=(784,), name="digits")
+    x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
+    x = layers.Dense(64, activation="relu", name="dense_2")(x)
+    outputs = layers.Dense(10, activation="softmax", name="predictions")(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def get_compiled_model():
+    model = get_uncompiled_model()
+    model.compile(
+        optimizer="rmsprop",
+        loss="sparse_categorical_crossentropy",
+        metrics=["sparse_categorical_accuracy"],
+    )
+    return model
+
+
+def custom_mean_squared_error(y_true, y_pred):
+    return tf.math.reduce_mean(tf.square(y_true - y_pred))
+
+
+class CustomMSE(keras.losses.Loss):
+    def __init__(self, regularization_factor=0.1, name="custom_mse"):
+        super().__init__(name=name)
+        self.regularization_factor = regularization_factor
+
+    def call(self, y_true, y_pred):
+        mse = tf.math.reduce_mean(tf.square(y_true - y_pred))
+        reg = tf.math.reduce_mean(tf.square(0.5 - y_pred))
+        return mse + reg * self.regularization_factor
+
+
+model = get_uncompiled_model()
+model.compile(optimizer=keras.optimizers.Adam(), loss=CustomMSE())
+
+y_train_one_hot = tf.one_hot(y_train, depth=10)
+model.fit(x_train, y_train_one_hot, batch_size=64, epochs=1)
+
+
+class CategoricalTruePositives(keras.metrics.Metric):
+    def __init__(self, name="categorical_true_positives", **kwargs):
+        super(CategoricalTruePositives, self).__init__(name=name, **kwargs)
+        self.true_positives = self.add_weight(name="ctp", initializer="zeros")
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.reshape(tf.argmax(y_pred, axis=1), shape=(-1, 1))
+        values = tf.cast(y_true, "int32") == tf.cast(y_pred, "int32")
+        values = tf.cast(values, "float32")
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, "float32")
+            values = tf.multiply(values, sample_weight)
+        self.true_positives.assign_add(tf.reduce_sum(values))
+
+    def result(self):
+        return self.true_positives
+
+    def reset_state(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.true_positives.assign(0.0)
+
+
+model = get_uncompiled_model()
+model.compile(
+    optimizer=keras.optimizers.RMSprop(learning_rate=1e-3),
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[CategoricalTruePositives()],
+)
+model.fit(x_train, y_train, batch_size=64, epochs=3)
+
+model = get_compiled_model()
+
+# First, let's create a training Dataset instance.
+# For the sake of our example, we'll use the same MNIST data as before.
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+# Shuffle and slice the dataset.
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(64)
+
+# Now we get a test dataset.
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+test_dataset = test_dataset.batch(64)
+
+# Since the dataset already takes care of batching,
+# we don't pass a `batch_size` argument.
+model.fit(train_dataset, epochs=3)
+
+# You can also evaluate or predict on a dataset.
+print("Evaluate")
+result = model.evaluate(test_dataset)
+dict(zip(model.metrics_names, result))
+
+image_input = keras.Input(shape=(32, 32, 3), name="img_input")
+timeseries_input = keras.Input(shape=(None, 10), name="ts_input")
+
+x1 = layers.Conv2D(3, 3)(image_input)
+x1 = layers.GlobalMaxPooling2D()(x1)
+
+x2 = layers.Conv1D(3, 3)(timeseries_input)
+x2 = layers.GlobalMaxPooling1D()(x2)
+
+x = layers.concatenate([x1, x2])
+
+score_output = layers.Dense(1, name="score_output")(x)
+class_output = layers.Dense(5, name="class_output")(x)
+
+model = keras.Model(
+    inputs=[image_input, timeseries_input], outputs=[score_output, class_output]
+)
