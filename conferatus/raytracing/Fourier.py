87b40e3e7fdf7f4895dd5ce751593d73db21b1dd
ttrubinov <t.trubinov@g.nsu.ru>
2023-04-01 15:34:14 +0700

    FFT slice

--- conferatus/raytracing/Fourier.py
+++ conferatus/raytracing/Fourier.py
@@ -5,17 +5,18 @@ from scipy.fft import fft, fftfreq
 
 class Fourier:
     @staticmethod
-    def fft(array: list[list[float]]) -> list[list[complex]]:
-        return list(map(lambda data: fft(np.array(data)), array))
+    def fft(array: list[list[float]], result_size: int = 250) -> list[list[complex]]:
+        return list(map(lambda data: fft(np.array(data))[:result_size], array))
 
     @staticmethod
     def fft_freq(array: list[list[float]], sampleSpacing: float = 1 / 12000) -> list[list[complex]]:
         return list(map(lambda data: fftfreq(len(data), sampleSpacing)[:len(data) // 2], array))
 
     @staticmethod
-    def get_amplitudes_and_phases(data: list[list[float]], flag: bool = False) -> (ndarray, ndarray) | ndarray:
+    def get_amplitudes_and_phases(data: list[list[float]],
+                                  whether_to_calculate_phases: bool = False) -> (ndarray, ndarray):
         fft_res = fft(data)
-        if flag:
+        if whether_to_calculate_phases:
             return np.abs(fft_res), np.angle(fft_res)
         else:
             return np.abs(fft_res)
