9cc7ce17176d621b360910085a52cb0115758db5
ttrubinov <t.trubinov@g.nsu.ru>
2023-04-03 17:23:20 +0700

    Directories

--- conferatus/dataset_recorder.py
+++ conferatus/dataset_recorder.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtCore, QtWidgets, uic
+from PyQt6 import QtWidgets
 import sys
 
 from ui.QuestionDialogView import QuestionDialogView
@@ -24,3 +24,26 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    # T = 1 / 12000
+    # data = [[], [], []]
+    # with open("oldExamples/samples/waiting505.txt", mode="r") as f:
+    #     s = f.readline()
+    #     nn = 250
+    #     counter = 0
+    #     while True:
+    #         s = f.readline()
+    #         if "End" in s:
+    #             break
+    #         if counter % nn == 0:
+    #             for i in data:
+    #                 i.append([])
+    #         s = s.split()
+    #         for i in range(0, 3):
+    #             data[i][counter // nn].append(float(s[i]))
+    #         counter += 1
+    #
+    # fourierSample = Fourier.get_amplitudes_and_phases([data[0][0], data[1][0], data[2][0]])
+    # Plotter.draw(fourierSample, color=["red", "blue", "green"])
+
+
