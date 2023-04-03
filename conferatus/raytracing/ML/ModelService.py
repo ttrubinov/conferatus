ac904c15693414f11f4ddcdd364124a704554d65
ttrubinov <t.trubinov@g.nsu.ru>
2023-04-03 17:23:20 +0700

    Directories

---
+++ conferatus/raytracing/ML/ModelService.py
@@ -0,0 +1,15 @@
+from conferatus.conferatus.raytracing.ML.Dataset import Dataset
+
+
+class ModelService:
+    @staticmethod
+    def train_model(data: Dataset) -> None:
+        return None
+
+    @staticmethod
+    def get_angle(data) -> int:
+        return 0
+
+    @staticmethod
+    def get_person(data) -> str:
+        return "Aboba"
