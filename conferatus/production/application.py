fc401a8862e4263fcc4fd907187072165a02c1b6
ttrubinov <t.trubinov@g.nsu.ru>
2023-04-05 09:05:15 +0700

    Full application

--- conferatus/production/application.py
+++ conferatus/production/application.py
@@ -1,5 +1,7 @@
-from conferatus.Arduino.ArduinoController import ArduinoController
-from conferatus.ML.core.ModelPredict import ModelPredict
+from conferatus.conferatus.Arduino.ArduinoController import ArduinoController
+from conferatus.conferatus.ML.core.ModelPredict import ModelPredict
+from conferatus.conferatus.Maths.Fourier import Fourier
+from conferatus.conferatus.Presenter.Presenter import Presenter
 
 dir_path = ""
 prefix = ""
@@ -9,9 +11,15 @@ data_size = 250
 
 if __name__ == '__main__':
     model_predict = ModelPredict.download(dir_path, prefix)
+    presenter = Presenter()
     while True:
         arduino_controller = ArduinoController(1, port=port, data_size=data_size)
-        sample = arduino_controller.recordData()[0]
-
-
-
+        data = arduino_controller.recordData()[0]
+        fft = Fourier.fft(data, result_size=100)
+        neuro = model_predict.predict_all(fft)
+        print(neuro)
+        if neuro["class"] == "bad_data":
+            print("BAD DATA")
+            continue
+        res_angle = neuro["angle"]
+        presenter.rotation(res_angle)
