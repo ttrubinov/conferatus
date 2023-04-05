c1955ac1e0e60301fe8491205df4e8c094580382
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-04-05 09:49:56 +0700

    fix moving sketch

--- conferatus/Motion/MotionController.py
+++ conferatus/Motion/MotionController.py
@@ -1,11 +1,19 @@
+import time
 import serial
 
 
 class MotionController:
-    def __init__(self, baud_rate=230400, port: str = f"COM3"):
+    def __init__(self, baud_rate=9600, port: str = f"/dev/ttyUSB0"):
         self.port = port
         self.baud_rate = baud_rate
-        self.serial = serial.Serial(self.port, self.baud_rate, timeout=2)
+        self.ser = serial.Serial(self.port, self.baud_rate, timeout=2)
 
     def move(self, angle: int) -> None:
-        self.serial.write((180 - angle).to_bytes(1, 'little'))
+        self.ser.write((180 - angle).to_bytes(1, 'little'))
+        time.sleep(3)
+
+# a = MotionController()
+
+# while True:
+#     a.move(90)
+#     a.move(180)
