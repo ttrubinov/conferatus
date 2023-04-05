a438cb08f449797f6fac19663c0effb19c3f312c
ttrubinov <t.trubinov@g.nsu.ru>
2023-04-05 10:10:59 +0700

    Name

--- conferatus/ui/RecordingModel.py
+++ conferatus/ui/RecordingModel.py
@@ -13,7 +13,7 @@ class RecordingThread(QtCore.QThread):
         super(RecordingThread, self).__init__(parent)
         self.__recordingPresenter__ = recordingPresenter
         self.__params__ = params
-        
+
     def run(self):
         with ArduinoController(batch_size = self.__params__.batchSize, port = self.__params__.port) as arduinoController:
             data = arduinoController.recordData()
@@ -23,12 +23,12 @@ class RecordingThread(QtCore.QThread):
         for batch in data:
             fourierSample = list(Fourier.get_amplitudes_and_phases(batch))
             Plotter.draw(fourierSample, color = ['green', 'midnightblue', 'red'], file_path = 'Maths/plot/fig.png',
-                         x_label="frequency", y_label="CU")
+                         x_label="frequency", y_label="CU", name="Frequency response")
 
             sample_ok : bool = self.__recordingPresenter__.processSampleDialog('Maths/plot/fig.png')
             if sample_ok:
-                samples.append(Sample(signals = fourierSample, 
-                                    angle = self.__params__.angle, 
+                samples.append(Sample(signals = fourierSample,
+                                    angle = self.__params__.angle,
                                     bad_data = False,
                                     frequency = self.__params__.frequency,
                                     person = self.__params__.person))
