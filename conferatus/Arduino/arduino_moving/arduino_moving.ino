e4181e38d2430ba0b323ace22a9fb35cd44dfcd0
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-05-09 19:31:30 +0700

    чето менял

--- conferatus/Arduino/arduino_moving/arduino_moving.ino
+++ conferatus/Arduino/arduino_moving/arduino_moving.ino
@@ -8,6 +8,8 @@ void setup() {
     servo.attach(2, 1000, 2000);        // привод на d2
     servo.setSpeed(60);
     servo.setAccel(0.2);
+
+    servo.setTargetDeg(90);
 }
 
 void loop() {
@@ -15,11 +17,13 @@ void loop() {
     
     if (Serial.available() > 0) {
         targetAngle = Serial.read();
-        if (targetAngle >= 0) {
+        if (targetAngle >= 0 && targetAngle != 10) {
           servo.setTargetDeg(targetAngle);
-          Serial.print(targetAngle);
+          Serial.println(targetAngle);
         }
     }
+//    Serial.print(23);
+//    servo.setTargetDeg(60);
 
-    delay(2);
+    delay(20);
 }
