4f584e996dacab09dbf754767ad27c50f2dae563
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-05-21 20:48:18 +0700

    аттач неправильный был

--- conferatus/Arduino/arduino_moving/arduino_moving.ino
+++ conferatus/Arduino/arduino_moving/arduino_moving.ino
@@ -4,26 +4,32 @@ ServoSmooth servo;
 uint8_t targetAngle;
 
 void setup() {
-    Serial.begin(9600);
-    servo.attach(2, 1000, 2000);        // привод на d2
-    servo.setSpeed(60);
+    Serial.begin(230400);
+//    servo.attach(2, 1000, 2000);        // привод на d2
+    servo.attach(2);
+    servo.setSpeed(120);
     servo.setAccel(0.2);
 
     servo.setTargetDeg(90);
 }
 
+//uint32_t tmr;
+//boolean flag;
+
 void loop() {
-    servo.tick();
-    
     if (Serial.available() > 0) {
         targetAngle = Serial.read();
-        if (targetAngle >= 0 && targetAngle != 10) {
+        if (targetAngle >= 0 && targetAngle <= 180) {
           servo.setTargetDeg(targetAngle);
           Serial.println(targetAngle);
         }
     }
-//    Serial.print(23);
-//    servo.setTargetDeg(60);
 
-    delay(20);
+//if (millis() - tmr >= 4000) {   // каждые 3 сек
+//    tmr = millis();
+//    flag = !flag;
+//    servo.setTargetDeg(flag ? 0 : 180);  // двигаем на углы 50 и 120
+//  }
+//
+    servo.tick();
 }
